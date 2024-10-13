620599b0eb452c54aa2c43c5ab5def4af8b6bf32
IcpJ42 <iblion08@gmail.com>
2024-10-13 18:10:04 +0700

    Сделал 4 задание

--- task4.py
+++ task4.py
@@ -1,4 +1,14 @@
 # Напишите функцию, которая находит
 # наибольший общий делитель двух натуральных чисел
 # На входе два числа, на выходе их НОД.
+def gcd(a, b):
+     a, b = max(a, b), min(a, b)
+     if b == 0:
+         return a
+     if a % b == 0:
+         return b
+     return gcd(a % b, b)
 
+a = int(input())
+b = int(input())
+print(f'НОД: {gcd(a, b)}')
