9d16f2a33a93613e65e4c93f555cd510a600e54f
IcpJ42 <iblion08@gmail.com>
2024-10-14 12:53:28 +0700

    Сделал 9 задание

--- task9.py
+++ task9.py
@@ -3,4 +3,16 @@
 # или слово NO в противном случае.
 # Операцией возведения в степень пользоваться нельзя!
 # Задача на рекурсию!
+def is_degree_2(n):
+    if n == 1:
+        return True
+    if n % 2 == 0:
+        return is_degree_2(n // 2)
+    return False
 
+
+n = int(input())
+if is_degree_2(n):
+    print('YES')
+else:
+    print('NO')
