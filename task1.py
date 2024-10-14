52dfc5161977d46967b1b1b29eecac3cc3e18cf8
IcpJ42 <iblion08@gmail.com>
2024-10-14 13:32:46 +0700

    Переделал 1 задание

--- task1.py
+++ task1.py
@@ -10,6 +10,10 @@
 # ********
 # *********
 # **********
+def triangle(n):
+    for i in range(1, n + 1):
+        print('*' * i)
+
+
 n = int(input())
-for i in range(1, n + 1):
-    print('*' * i)
+triangle(n)
